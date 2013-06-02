8757472d3dc6364d572397c9492b994f81bd4cf7
Sergiusz Urbaniak <sergiusz.urbaniak@gmail.com>
2013-06-02 18:30:01 +0200

    renamed NewEelAndRabbit1 to NewEelAndRabbit7

--- topcoder/eelandrabbit/eelandrabbit_test.go
+++ topcoder/eelandrabbit/eelandrabbit_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func NewEelAndRabbit1() *EelAndRabbit {
+func NewEelAndRabbit7() *EelAndRabbit {
 	l := []int{2, 4, 3, 2, 2, 1, 10}
 	t := []int{2, 6, 3, 7, 0, 2, 0}
 
@@ -12,28 +12,28 @@ func NewEelAndRabbit1() *EelAndRabbit {
 }
 
 func TestCatchOne(tst *testing.T) {
-	m := NewEelAndRabbit1().CatchOne()
+	m := NewEelAndRabbit7().CatchOne()
 	if m != 4 {
 		tst.Errorf("got %d, expected %d", m, 4)
 	}
 }
 
 func TestCatchTwo(tst *testing.T) {
-	m := NewEelAndRabbit1().CatchTwo()
+	m := NewEelAndRabbit7().CatchTwo()
 	if m != 6 {
 		tst.Errorf("got %d, expected %d", m, 6)
 	}
 }
 
 func BenchmarkCatchOne(b *testing.B) {
-	e := NewEelAndRabbit1()
+	e := NewEelAndRabbit7()
 	for i := 0; i < b.N; i++ {
 		e.CatchOne()
 	}
 }
 
 func BenchmarkCatchTwo(b *testing.B) {
-	e := NewEelAndRabbit1()
+	e := NewEelAndRabbit7()
 	for i := 0; i < b.N; i++ {
 		e.CatchTwo()
 	}
