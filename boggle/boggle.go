c340271735839bb60083aae4305f7c3412ac9712
Sergiusz Urbaniak <sergiusz.urbaniak@gmail.com>
2013-06-06 10:26:45 +0200

    boggle refactorings

--- boggle/boggle.go
+++ boggle/boggle.go
@@ -1,69 +1,91 @@
 package boggle
 
 type Game struct {
-	letters [][]rune
 	size    BoardSize
+	adj     [][]Vertex
+	letters []rune
 }
 
 type BoardSize struct {
 	m, n int
 }
 
-type Pos struct {
-	x, y int
-}
+type Vertex int
 
-func (p *Pos) IsValid(s *BoardSize) bool {
-	return p.x >= 0 && p.y >= 0 && p.x < s.n && p.y < s.m
-}
-
-func (g *Game) IterNeighbours(p Pos) chan Pos {
-	ch := make(chan Pos)
-
-	if !p.IsValid(&g.size) {
-		close(ch)
+func NewVertex(i int, s *BoardSize) (Vertex, bool) {
+	if i >= 0 && i < s.m*s.n {
+		return Vertex(i), true
 	} else {
-		go func() {
-			for y := p.y - 1; y <= p.y+1; y++ {
-				for x := p.x - 1; x <= p.x+1; x++ {
-					n := Pos{x, y}
-					if n.IsValid(&g.size) && n != p {
-						ch <- n
-					}
+		return -1, false
+	}
+}
+
+func NewVertexFromCoords(x, y int, s *BoardSize) (Vertex, bool) {
+	if x >= 0 && y >= 0 && x < s.n && y < s.m {
+		return Vertex(y*s.m + x), true
+	} else {
+		return -1, false
+	}
+}
+
+func (v *Vertex) IterNeighbours(s *BoardSize) chan Vertex {
+	ch := make(chan Vertex)
+
+	vx := int(*v) % s.n
+	vy := (int(*v) - vx) / s.m
+
+	go func() {
+		for y := -1; y <= 1; y++ {
+			for x := -1; x <= 1; x++ {
+				n, valid := NewVertexFromCoords(vx+x, vy+y, s)
+				if valid && n != *v {
+					ch <- n
 				}
 			}
+		}
 
-			close(ch)
-		}()
-	}
+		close(ch)
+	}()
 
 	return ch
 }
 
-func (g *Game) Init(rows []string) *Game {
-	row_cnt := len(rows)
-	if row_cnt < 1 {
+func (g *Game) Init(words []string) *Game {
+	rows := len(words)
+	if rows < 1 {
 		return nil
 	}
 
-	g.letters = make([][]rune, row_cnt)
+	cols := 0
+	for m := 0; m < rows; m++ {
+		row := []rune(words[m])
 
-	col_cnt := 0
-	for m := 0; m < row_cnt; m++ {
-		row := []rune(rows[m])
-		g.letters[m] = row
+		if cols == 0 {
+			cols = len(row)
+			size := rows * cols
 
-		if col_cnt == 0 {
-			col_cnt = len(row)
-		} else if col_cnt != len(row) {
+			g.letters = make([]rune, size)
+			g.adj = make([][]Vertex, size)
+			g.size = BoardSize{rows, cols}
+		} else if cols != len(row) {
 			return nil
+		}
+
+		for n := 0; n < cols; n++ {
+			i := m*rows + n
+			g.letters[i] = row[n]
+
+			g.adj[i] = make([]Vertex, 0)
+			v := Vertex(i)
+			for a := range v.IterNeighbours(&g.size) {
+				g.adj[i] = append(g.adj[i], a)
+			}
 		}
 	}
 
-	g.size = BoardSize{row_cnt, col_cnt}
 	return g
 }
 
-func New(rows []string) *Game {
+func NewGame(rows []string) *Game {
 	return new(Game).Init(rows)
 }
